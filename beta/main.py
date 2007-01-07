a01bc13ddb136c0b6b8f26fcb072d37f79f54fe3
root <root@arietta.(none)>
2007-01-07 09:42:51 +0100

    main.py is written using sheculer and threading. It works but the inherend delays in the system are too much for the frequencies we are measureing

--- beta/main.py
+++ beta/main.py
@@ -1,19 +1,29 @@
-from logging import *
+import datetime
+import time
+import max11040klib
 import threading
+import sched
+
+scheduler = sched.scheduler(time.time, time.sleep)
 
 m = max11040klib.MaxSpiDev(0)
 m.set_registers("config.ini")
-logged = datetime.datetime.now().date() - datetime.timedelta(days=1)
 
-while True:
-    while datetime.datetime.now().date() != logged:
-        now = datetime.datetime.now()
-        # end_time = (datetime.datetime.now() + datetime.timedelta(days=1)).replace(hour=0, minute=0, second=0)
-        end_time = now + datetime.timedelta(seconds=1)
-        logged = now.date()
-        data_file_name = datetime.datetime.now().strftime("%Y%j-%H%M%S.dat")
-        log_book = open(data_file_name, "w")
-        t = threading.Thread(target=day_log, args=(log_book, m, end_time))
-        t.start()
-        time.sleep((end_time-now).total_seconds())
-        exit()
+log_book = open(datetime.datetime.now().strftime("%Y%j-%H%M%S.dat"), "w")
+
+
+def log(data_file, m_spi, delay=0.01):
+    threading.Timer(delay, log, [data_file, m_spi]).start()
+    x, y, z, a = m_spi.read_adc_data()
+    data_file.write(str(x)+"\t"+str(y)+"\t"+str(x)+"\t"+str(a)+"\n")
+
+
+def main(data_file, m_spi):
+    log(data_file, m_spi)
+    next_datetime = datetime.datetime.now() + datetime.timedelta(days=1)
+    data_file = open(next_datetime.strftime("%Y%j-%H%M%S.dat"), "w")
+    next_schedule = next_datetime.replace(hour=0, minute=0, second=0)
+    scheduler.enterabs(time.mktime(next_schedule.timetuple()), 1, log, (data_file, m_spi,))
+
+main(log_book, m)
+scheduler.run()
